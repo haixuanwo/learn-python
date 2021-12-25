69edd743082ea3806a04694f7745bd3ae3fb7812
haixuanwo <haixuanwo_clark@hotmail.com>
2021-12-25 09:31:48 +0800

    turtle画图包

---
+++ learn-100-days/01-basic/01_hello.py
@@ -0,0 +1,18 @@
+'''
+Author: Clark
+Email: haixuanwoTxh@gmail.com
+Date: 2021-12-24 20:22:04
+LastEditors: Clark
+LastEditTime: 2021-12-24 20:29:14
+Description: file content
+'''
+
+import sys
+
+print('hello, world!')
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
+
+print(sys.version_info)
+print(sys.version)
---
+++ learn-100-days/01-basic/02_turtle.py
@@ -0,0 +1,23 @@
+'''
+Author: Clark
+Email: haixuanwoTxh@gmail.com
+Date: 2021-12-24 20:30:34
+LastEditors: Clark
+LastEditTime: 2021-12-24 20:32:37
+Description: file content
+'''
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
